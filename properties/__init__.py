4937cf2cfa2b3f5c024ac0e67a63d1ac9e1e0c30
Agnes Nduta <47296541+AgnesNM@users.noreply.github.com>
2025-07-13 19:15:32 +0300

    Create __init__.py

---
+++ properties/__init__.py
@@ -0,0 +1,8 @@
+"""
+Properties app initialization.
+
+This module configures the properties app to use the custom PropertiesConfig
+which includes signal handler registration for automatic cache invalidation.
+"""
+
+default_app_config = 'properties.apps.PropertiesConfig'
